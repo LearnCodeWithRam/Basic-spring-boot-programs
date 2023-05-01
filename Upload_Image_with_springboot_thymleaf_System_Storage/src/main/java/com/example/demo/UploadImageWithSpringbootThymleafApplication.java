0495f0589ea81998315b3078cd24f2dac5b520b0
Ramanuj <ramanujsaket1996@gmail.com>
2023-05-01 13:11:15 +0530

    first day commit
    rty

---
+++ Upload_Image_with_springboot_thymleaf_System_Storage/src/main/java/com/example/demo/UploadImageWithSpringbootThymleafApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadImageWithSpringbootThymleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadImageWithSpringbootThymleafApplication.class, args);
+	}
+
+}
